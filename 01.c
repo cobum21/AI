c73f656f805b35f477440f54539daf6a591cd9fb
cobum21 <90166523+cobum21@users.noreply.github.com>
2021-09-06 12:55:51 +0900

    Create 01.c
    첫번째공부

---
+++ 01.c
@@ -0,0 +1,44 @@
+//#include <stdio.h>
+//#define SIZE 6
+//int main(void)
+//{
+//
+//	double score[] = { 89.3, 79.2, 84.83, 76.8, 92.52, 97.4  };
+//	double sum = 0;
+//
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		sum += score[i];
+//		printf("score[%d] = %.2f\n", i, score[i] );
+//	}
+//	printf("성적의 합은 %.2f이고 평균은 %2f이다.\n", sum, sum/SIZE);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+//{
+//	int input[20] = { 0, };
+//	printf("배열에 저장할 정수를 여러 개 입력하시오.");
+//	printf("0을 입력하면 입력을 종료합니다.\n");
+//	int i = 0;
+//	printf("input : \n");
+//	do {
+//		scanf_s("%d", &input[i]);
+//	} while (input[i++] != 0);
+//
+//	i = 0;
+//	while (input [i] != 0) {
+//		printf("%d", input[i++]);
+//	} 
+//	puts("");
+//
+//	return 0;
+//}
+
+
+
